d668414854d03ed9ccb9f16b9dda7166c9266a9f
dallanj <dallanjones@pm.me>
2022-01-25 02:04:22 -0500

    started listing page content from database

--- resources/views/livewire/frontpage.blade.php
+++ resources/views/livewire/frontpage.blade.php
@@ -878,6 +878,7 @@
 </div>
   
 @endif
+{!! html_entity_decode($content) !!}
 <!-- CTA -->
 <div class="flex flex-col bg-cta-beige relative overflow-hidden">
   <div class="max-w-7xl mx-auto py-12 px-4 sm:px-6 lg:py-16 lg:px-8 lg:items-center lg:justify-between position:relative z-10">
